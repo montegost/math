b457ba0261350c404738211282500fb8b5802ba7
montegost <138040250+montegost@users.noreply.github.com>
2026-04-21 10:36:20 +0500

    feat!: replace error printing with exceptions

--- include/math/math.h
+++ include/math/math.h
@@ -1,144 +1,107 @@
-#include <cstdio>
-#include <cstdlib>
 #include <climits>
-#include <cerrno>
-#include <cstring>
+#include <stdexcept>
 
-namespace math {
-
-inline bool addition( long firstValue, long secondValue, long* res )
+namespace math
 {
-    long result;
-    if (__builtin_add_overflow(firstValue, secondValue, &result))
-    {
-        printf("Addition %ld + %ld makes overflow!\n", firstValue, secondValue);
-        return false;
-    }
-    if (res == nullptr){
-        printf("res must be not nullptr");
-        return false;
-    }
-    
-    *res = result;
-    
-    return true;
-}
 
-inline bool subtraction( long firstValue, long secondValue, long* res )
+inline long addition( long firstValue, long secondValue )
 {
-    long result;
-    if (__builtin_sub_overflow(firstValue, secondValue, &result))
-    {
-        printf("Subtraction %ld - %ld makes overflow!\n", firstValue, secondValue);
-        return false;
-    }
-    if (res == nullptr){
-        printf("res must be not nullptr");
-        return false;
-    }
-    
-    *res = result;
-    
-    return true;
+     long result;
+     if( __builtin_add_overflow( firstValue, secondValue, &result ) )
+     {
+          throw std::overflow_error( "Addition overflows" );
+     }
+
+     return result;
 }
 
-inline bool multiplication(long firstValue, long secondValue, long* res) {
-    long result;
-    if (__builtin_mul_overflow(firstValue, secondValue, &result))
-    {
-        printf("Multiplication %ld * %ld makes overflow!\n", firstValue, secondValue);
-        return false;
-    }
-    if (res == nullptr){
-        printf("res must be not nullptr");
-        return false;
-    }
-    
-    *res = result;
-    
-    return true;
+inline long subtraction( long firstValue, long secondValue )
+{
+     long result;
+     if( __builtin_sub_overflow( firstValue, secondValue, &result ) )
+     {
+          throw std::overflow_error( "Subtraction overflows" );
+     }
+
+     return result;
 }
 
-inline bool division(long firstValue, long secondValue, long* res) {
-    if (secondValue == 0) {
-        printf("Division on 0\n");
-        return false;  // Ошибка: деление на ноль
-    }
-    
-    // Проверка переполнения: LONG_MIN / -1
-    // Так как LONG_MIN = -9223372036854775808
-    // А LONG_MAX = 9223372036854775807
-    // -LONG_MIN не помещается в long
-    if (firstValue == LONG_MIN && secondValue == -1) {
-        printf("Division %ld / %ld makes overflow!\n", firstValue, secondValue);
-        return false;
-    }
-    
-    if (res == nullptr){
-        printf("res must be not nullptr");
-        return false;
-    }
-    
-    *res = firstValue / secondValue;
-    return true;
+inline long multiplication( long firstValue, long secondValue )
+{
+     long result;
+     if( __builtin_mul_overflow( firstValue, secondValue, &result ) )
+     {
+          throw std::overflow_error( "Multiplication overflows" );
+     }
+
+     return result;
 }
 
-inline bool power( long base, long exponent, long* res) {
-    if (res == nullptr){
-        printf("res must be not nullptr");
-        return false;
-    }
+inline long division( long firstValue, long secondValue )
+{
+     if( secondValue == 0 )
+     {
+          throw std::invalid_argument( "Division on 0" );
+     }
 
-    if (exponent == 0) {
-        *res = 1;
-        return true;
-    }
-    
-    if (exponent < 0) {
-        printf("exponent must be not negative");
-        return false;
-    }
-    
-    long result = 1;
-    
-    for (long i = 0; i < exponent; i++) {
-        if (__builtin_mul_overflow(result, base, &result))
-        {
-            printf("Power %ld ^ %ld makes overflow!\n", base, exponent);
-            return false;
-        }
-    }
-    
+     // Проверка переполнения: LONG_MIN / -1
+     // Так как LONG_MIN = -9223372036854775808
+     // А LONG_MAX = 9223372036854775807
+     // -LONG_MIN не помещается в long
+     if( firstValue == LONG_MIN && secondValue == -1 )
+     {
+          throw std::overflow_error( "Division overflows" );
+     }
 
-    *res = result;
-    
-    return true;
+     return firstValue / secondValue;
 }
 
-inline bool factorial(long firstValue, long* result) {
-    if (firstValue < 0)
-    {
-        printf("factorial must be not negative. %ld\n", firstValue);
-        return false;
-    }
-    
-    if (firstValue == 0 || firstValue == 1) {
-        *result = 1;
-        return true;
-    }
-    
-    long sub_result;
-    if (!factorial(firstValue - 1, &sub_result)) {
-        return false;  // Переполнение в рекурсивном вызове
-    }
-    
-    if (__builtin_mul_overflow(sub_result, firstValue, result)) {
-        printf("Factorial makes overflow!\n");
-        return false;
-    }
-    
-    return true;
+inline long power( long base, long exponent )
+{
+     if( exponent == 0 )
+     {
+          return 1;
+     }
+
+     if( exponent < 0 )
+     {
+          throw std::invalid_argument( "exponent must be not negative" );
+     }
+
+     long result = 1;
+
+     for( long i = 0; i < exponent; i++ )
+     {
+          if( __builtin_mul_overflow( result, base, &result ) )
+          {
+               throw std::overflow_error( "Power overflows" );
+          }
+     }
+
+     return result;
 }
 
+inline long factorial( long firstValue )
+{
+     if( firstValue < 0 )
+     {
+          throw std::invalid_argument( "Factorial argument must be non-negative" );
+     }
 
-} // namespace math
+     if( firstValue == 0 || firstValue == 1 )
+     {
+          return 1;
+     }
+
+     long sub_result = factorial( firstValue - 1 );
+     long result;
+
+     if( __builtin_mul_overflow( sub_result, firstValue, &result ) )
+     {
+          throw std::overflow_error( "Factorial overflows" );
+     }
+
+     return result;
+}
+
+}// namespace math
